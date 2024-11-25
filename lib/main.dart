096c7405d863f053202de0613458d95b6c254694
ishakper <ishakhadipernama@gmail.com>
2024-11-25 10:10:29 +0700

    W11: Soal 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 import 'package:async/async.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Future Demo',
+      title: 'Moch. Dzaky Musyaddad',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -32,12 +34,51 @@ class FuturePage extends StatefulWidget {
 }
 
 class _FuturePageState extends State<FuturePage> {
-  Future<Response> getData() async {
-    const authority = 'www.googleapis.com';
-    const path = '/books/v1/volumes/e-ZDDwAAQBAJ';
-    Uri url = Uri.https(authority, path);
-    return http.get(url);
+  void returnFG() {
+    final futures = Future.wait<int>([
+      returnOneAsync(),
+      returnTwoAsync(),
+      returnThreeAsync(),
+    ]);
+
+    // FutureGroup<int> futureGroup = FutureGroup<int>();
+    // futureGroup.add(returnOneAsync());
+    // futureGroup.add(returnTwoAsync());
+    // futureGroup.add(returnThreeAsync());
+    // futureGroup.close();
+    // futureGroup.future.then((List <int> value) {
+    //   int total = 0;
+    //   for (var element in value) {
+    //     total += element;
+    //   }
+    //   setState(() {
+    //     result = total.toString();
+    //   });
+    // });
   }
+
+
+
+
+  late Completer completer;
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    try {
+      await Future.delayed(const Duration(seconds : 5));
+      completer.complete(42);
+    }
+    catch (_) {
+      completer.completeError({});
+    }
+  }
+
+
 
   Future<int> returnOneAsync() async {
     await Future.delayed(const Duration(seconds: 3));
@@ -64,55 +105,59 @@ class _FuturePageState extends State<FuturePage> {
     });
   }
 
-  late Completer completer;
-
-  Future getNumber() {
-    completer = Completer<int>();
-    calculate();
-    return completer.future;
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/MWW8DwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
   }
-
-  Future calculate() async {
-    try { 
-      await Future.delayed(const Duration(seconds: 5));
-    completer.complete(42);
-  }
-  catch (_) {
-    completer.completeError({});
-  } }
 
   String result = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Back from the future'),
+        title: const Text('Back from the Future'),
       ),
       body: Center(
-        child: Column(children: [
-          const Spacer(),
-          ElevatedButton(
-            child: const Text('GO!'),
-            onPressed: () {
-              count();
-              setState(() {});
-              getNumber().then((value) {
-                  setState(() { 
-                    result = value.toString();
-                    });
-                 
-              }).catchError((e) {
-                result = 'An error occurred';
-                setState(() {});
-              });
-            },
-          ),
-          const Spacer(),
-          Text(result),
-          const Spacer(),
-          const CircularProgressIndicator(),
-          const Spacer(),
-        ]),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {
+                returnFG();
+                // Praktikum 3
+                // getNumber().then((value) {
+                //   setState(() {
+                //     result = value.toString();
+                //   });
+                // }).catchError((e) {
+                //   result = 'An error occured';
+                // });
+
+                // Praktikum 2
+                // count();
+
+
+                // Praktikum 1
+                // setState(() {});
+                // getData()
+                // .then((value) {
+                //   result = value.body.toString().substring(0, 450);
+                //   setState(() {});
+                // }).catchError((_) {
+                //   result = 'An error occured';
+                //   setState(() {});
+                // });
+                }),
+              const Spacer(),
+              Text(result),
+              const Spacer(),
+              const CircularProgressIndicator(),
+              const Spacer(),
+          ],
+        ),
       ),
     );
   }
